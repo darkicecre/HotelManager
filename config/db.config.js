298d36a0bc2273692636bf68083365ef8e99bb06
darkicecre <vophiminhhieuthukhoanghia@gmail.com>
2021-12-03 12:11:15 +0700

    Update Template

---
+++ config/db.config.js
@@ -0,0 +1,15 @@
+const Pool = require("mongodb").Pool;
+
+module.exports = new Pool({
+    // user: "",
+    // password: "",
+    // host: "",
+    // port: ,
+    // database: "",
+    // pool: {
+    //     max: ,
+    //     min: ,
+    //     acquire: ,
+    //     idle: 
+    //   }
+})
